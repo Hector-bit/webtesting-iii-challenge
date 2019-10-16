87b0599c5a7df269e143c9d6070a0528e1448b7e
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-16 00:12:59 -0700

    added some more tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,9 +1,10 @@
 // Test away!
 import React from 'react';
-import { render } from 'react';
+// import { render } from 'react';
 import Dashboard from './Dashboard'
 import * as rtl from 'react-testing-library';
-import 'jest-dom/extend-expect';
+// import 'jest-dom/extend-expect';
+import { render, fireEvent } from 'react-testing-library';
 afterEach(rtl.cleanup);
 
 describe('Dashboard', () => {
@@ -11,12 +12,50 @@ describe('Dashboard', () => {
         const wrapper = rtl.render(<Dashboard/>);
         expect(wrapper.baseElement).toMatchSnapshot();
     })
-})
 
-describe('Dashboard', () => {
-    it('checks if the Dashboard is closed and locked', () => {
-        const { getByText } = rtl.render(<Dashboard/>);
-        getByText(/lock gate/i);
-        getByText(/close gate/i); 
+    it('default state is unlocked and open', () => {
+        const dashboard = render(<Dashboard/>);
+
+        const lockBtn = dashboard.getByText('Lock Gate');
+        const closeBtn = dashboard.getByText('Close Gate');
+
+        expect(lockBtn.disabled).toBe(true);
+        expect(closeBtn.disabled).toBe(false);
+    })
+
+    it('close gat to enable lcok btn', () => {
+        const dashboard = render(<Dashboard/>);
+        const closeBtn = dashboard.getByText('Close gate');
+
+        fireEvent.click(closeBtn);
+
+        const openBtn = dashboard.getByText('Open Gate');
+        const lockBtn = dashboard.getByText('Lock Gate');
+        expect(openBtn.disabled).toBe(false);
+        expect(lockBtn.disabled).toBe(false);
+
+    })
+
+    it('locks gate to disable open btn and enable unlocked', () => {
+        const dashboard = render(<Dashboard/>);
+
+        const closeBtn = dashboard.getByText('Close gate');
+        fireEvent.click(closeBtn);
+
+        const lockBtn = dashboard.getByText('Lock gate');
+        fireEvent.click(lockBtn);
+
+        const openBtn = dashboard.getByText('Open Gate');
+        const unlockBtn = dashboard.getByText('Unlock Gate');
+        expect(openBtn.disabled).toBe(true);
+        expect(unlockBtn.disabled).toBe(false);
     })
 })
+
+// describe('Dashboard', () => {
+//     it('checks if the Dashboard is closed and locked', () => {
+//         const { getByText } = rtl.render(<Dashboard/>);
+//         getByText(/lock gate/i);
+//         getByText(/close gate/i); 
+//     })
+// })
